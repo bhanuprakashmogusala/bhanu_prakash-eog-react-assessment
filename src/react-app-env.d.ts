689c25628a9d59a56c23b603479b352712580470
Bhanu Mogusala <bhanuprakashmogusala@Bhanuprakashs-MacBook-Pro.local>
2021-02-02 11:06:41 -0500

    Created Dashboard

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+declare module 'react-timeseries-charts';
+declare module 'pondjs';
+declare module 'react-select';
+declare module 'react-select/animated';
+declare module 'subscriptions-transport-ws';
+declare module 'redux';
